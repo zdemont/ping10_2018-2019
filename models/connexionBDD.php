51d05d8624a6671717fa22bcdf0b5a83fc28c263
yviouswilliamous <yviwilli@yahoo.fr>
2018-11-15 09:02:36 +0100

    pages php contenant les fonctions pour se connecter à la bdd(connexion.php) et pour authentifier l'utilisateur(connexionDAO.php)

--- models/connexionBDD.php
+++ models/connexionBDD.php
@@ -1,6 +1,6 @@
 <?php
 
-function bdd_connexion(){
+function bddConnexion(){
 
 $servername = "localhost";
 $username = "root";
@@ -18,33 +18,5 @@ echo "Connected successfully";
 
 return $conn;
 }
-
-
-//Cette fonction vérifie si le couple (id,mdp) saisi est présent dans la bdd
-function authentication($id,$password){
-
-    //new connection to database
-    $conn = bdd_connexion();
-
-    // prepare and bind
-    $stmt = $conn->prepare("SELECT * FROM USERS WHERE conn_id=? AND mdp=?");
-    $stmt->bind_param("ss", $id, $password);
-    
-    //execute and store the results
-    $stmt->execute();
-    $stmt->store_result();
-    $result = $stmt->num_rows; 
-
-    //close the connection
-    $stmt->close();
-    $conn->close();
-
-    //check the result of the query
-    if($result != 1)
-        return false;//renvoyer false si la requête ne renvoie aucune ligne
-    else
-        return true;//renvoyer true si la requête renvoie une ligne
-}
-
 
 ?>
--- models/connexionDAO.php
+++ models/connexionDAO.php
@@ -1,2 +1,31 @@
 <?php
-//créer route avec connexion vers la base de données
+
+include_once("connexionBDD.php");
+
+//Cette fonction vérifie si le couple (id,mdp) saisi est présent dans la bdd
+function authentication($id,$password){
+
+    //new connection to database
+    $conn = bddConnexion();
+
+    // prepare and bind
+    $stmt = $conn->prepare("SELECT * FROM USERS WHERE conn_id=? AND mdp=?");
+    $stmt->bind_param("ss", $id, $password);
+    
+    //execute and store the results
+    $stmt->execute();
+    $stmt->store_result();
+    $result = $stmt->num_rows; 
+
+    //close the connection
+    $stmt->close();
+    $conn->close();
+
+    //check the result of the query
+    if($result != 1)
+        return false;//renvoyer false si la requête ne renvoie aucune ligne
+    else
+        return true;//renvoyer true si la requête renvoie une ligne
+}
+
+?>
